f4711a82969faa4d2be7d91c2cc20bbaee9c21b1
Juliana Vaz <juliana.av.vaz@gmail.com>
2026-02-19 09:06:29 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,18 +1,23 @@
 import { test, expect } from '@playwright/test';
 
+//AAA - Arrange, Act, Assert
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  //Arrange
   await page.goto('http://localhost:5173/');
 
   //Checkpoint 1: A página deve estar online
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  
+
+  //Act
   //Checkpoint 2: A página deve acessar a página de consulta de pedidos
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
   await page.getByTestId('search-order-id').fill('VLO-0HBWAW');
   await page.getByTestId('search-order-button').click();
-  
+
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-0HBWAW');
 
